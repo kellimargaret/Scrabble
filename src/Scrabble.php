ca2aeb88b5eadb5c5cecb4cf22686e3920fc6d15
Casey Heitz & Kelli Sommerdyke <student@epicodus.com>
2015-08-13 10:35:03 -0700

    Add first four tests and Scrabble.php

---
+++ src/Scrabble.php
@@ -0,0 +1,45 @@
+<?php
+
+    class Scrabble
+    {
+        function scrabbleGame($input)
+        {
+            $score = 0;
+            $input_array = str_split($input);
+            foreach($input_array as $letter)
+            {
+                if (($letter == "a") || ($letter == "e") || ($letter == "i") || ($letter == "o") || ($letter == "u") || ($letter == "l") || ($letter == "r") || ($letter == "s") || ($letter == "t")) {
+                    $score += 1;
+                } elseif(($letter == "d") || ($letter == "g")) {
+                    $score += 2;
+                } elseif (($letter == "b") || ($letter == "c") || ($letter == "m") || ($letter == "p")) {
+                    $score += 3;
+                } elseif (($letter == "f") || ($letter == "h") || ($letter == "v") || ($letter == "w") || ($letter == "y")) {
+                    $score += 4;
+                } elseif (($letter == "k")) {
+                    $score += 5;
+                } elseif (($letter == "j") || ($letter == "x")) {
+                    $score += 8;
+                } elseif (($letter == "q") || ($letter == "z")) {
+                    $score += 10;
+                }
+            }
+            /*$score = 0;
+            $count = 0;
+            $letter_array = array(array("a", "e", "i", "o", "u", "l", "r", "s", "t") => 1);
+            echo $letter_array[0];
+            foreach($letter_array as $array)
+            {
+                foreach($array as $letters)
+                {
+                    if($input == $letters)
+                    {
+                        $score += $letter_array[$count];
+                    }
+                }
+                $count += 1;
+            }*/
+            return $score;
+        }
+    }
+ ?>
---
+++ tests/ScrabbleTest.php
@@ -0,0 +1,48 @@
+<?php
+
+    require_once "src/Scrabble.php";
+
+    class ScrabbleTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_one_letter()
+        {
+            $test_Scrabble = new Scrabble;
+            $input = "a";
+
+            $result = $test_Scrabble->scrabbleGame($input);
+
+            $this->assertEquals(1, $result);
+        }
+
+        function test_two_letters()
+        {
+            $test_Scrabble = new Scrabble;
+            $input = "ae";
+
+            $result = $test_Scrabble->scrabbleGame($input);
+
+            $this->assertEquals(2, $result);
+        }
+
+        function test_word()
+        {
+            $test_Scrabble = new Scrabble;
+            $input = "hello";
+
+            $result = $test_Scrabble->scrabbleGame($input);
+
+            $this->assertEquals(8, $result);
+        }
+
+        function test_upcase()
+        {
+            $test_Scrabble = new Scrabble;
+            $input = "Hello";
+
+            $result = $test_Scrabble->scrabbleGame($input);
+
+            $this->assertEquals(8, $result);
+        }
+    }
+?>
